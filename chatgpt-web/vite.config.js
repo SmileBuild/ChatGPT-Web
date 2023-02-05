e712d3f40f9771a153a07686bf43f2c2c740b487
Smile Way <bigx.w@foxmail.com>
2023-02-05 20:58:01 +0800

    chage root.

--- chatgpt-web/vite.config.js
+++ chatgpt-web/vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base:'/ChatGPT-Web/',
+  base:'/ChatGPT-Web/',
   build:{
   },
   // publicPath: process.env.NODE_ENV === "production" ? "/ChatGPT-Web/prod" : "/",
