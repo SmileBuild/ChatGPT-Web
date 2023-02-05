c23e7fa8b59855d55c5056fad7ec437bb1fd133a
Smile Way <bigx.w@foxmail.com>
2023-02-05 20:26:21 +0800

    modify website url

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f https://github.com/SmileBuild/ChatGPT-Web.git main:gh-pages
 
 cd -
