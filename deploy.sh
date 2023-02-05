66c654afb2c90f2c60f4fc572d0aaf47e9aeb4a1
Smile Way <bigx.w@foxmail.com>
2023-02-05 20:49:13 +0800

    modify root.

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# abort on errors
-set -e
+# # abort on errors
+# set -e
 
-# build
-npm run build
+# # build
+# npm run build
 
-# navigate into the build output directory
-cd chatgpt/dist
+# # navigate into the build output directory
+# cd chatgpt/dist
 
-# place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+# # place .nojekyll to bypass Jekyll processing
+# echo > .nojekyll
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-git init
-git checkout -B main
-git add -A
-git commit -m 'deploy'
+# git init
+# git checkout -B main
+# git add -A
+# git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# # if you are deploying to https://<USERNAME>.github.io
+# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/SmileBuild/ChatGPT-Web.git main:gh-pages
+# # if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f https://github.com/SmileBuild/ChatGPT-Web.git main:gh-pages
 
-cd -
+# cd -
