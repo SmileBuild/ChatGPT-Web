a60c37ea17f992d8faeb1494a8dd0ad074ccbb51
Smile Way <bigx.w@foxmail.com>
2023-02-13 21:55:11 +0800

    rebuild and fix some bug.

--- chatgpt-web/src/components/MobilePage.vue
+++ chatgpt-web/src/components/MobilePage.vue
@@ -1,66 +1,65 @@
 <template>
-    <el-container style="height:100vh">
-        <el-container>
-            <el-main>
-                <el-row>
-                    <el-col :span="2">
-                        <el-button size="large" type="primary" icon="Monitor" @click="jumpWeb" circle>
-                        </el-button>
-                    </el-col>
-                    <el-col :span="22"><el-form label-width="auto" size="large">
-                            <el-input v-model="api_key" placeholder="add key here. 在这里输入秘钥" show-password
-                                type="password"></el-input></el-form>
-                    </el-col>
-                </el-row>
-
-                <el-list>
-                    <el-list-item v-for="(chat, index) in chatList" :key="index">
-
-                        <!--判断消息是从自己发出还是接收-->
-                        <template v-if="chat.from === 'self'">
-                            <el-row :gutter="8" justify="end" style="margin-top: 15px;">
-
-                                <el-card>
-                                    <span style="white-space: pre-wrap; margin-right: 4px;">{{ chat.text }}</span>
-                                </el-card>
-                                <el-avatar style="margin-left:4px;" icon="UserFilled" />
-                            </el-row>
-                        </template>
-                        <template v-else>
-                            <el-row :gutter="8" justify="start" style="margin-top: 15px;">
-                                <el-avatar size="mini" style="margin-right: 4px;">
-                                    <img :src="chat.face" />
-                                </el-avatar>
-                                <el-card>
-                                    <span style="white-space: pre-wrap; margin-left: 4px;">{{ chat.text }}</span>
-                                </el-card>
-
-                            </el-row>
-                        </template>
-
-                    </el-list-item>
-                </el-list>
-            </el-main>
-            <el-footer>
-                <el-row justify="end">
+    <el-container style="height:90vh">
+        <el-header>
+            <el-row :gutter="8" justify="space-between">
+                <el-col :span="4">
+                    <el-button size="large" type="primary" icon="Monitor" @click="jumpWeb" circle>
+                    </el-button>
+                </el-col>
+                <el-col :span="16"><el-form label-width="auto" size="large">
+                        <el-input v-model="api_key" placeholder="add key here. 在这里输入秘钥" show-password
+                            type="password"></el-input></el-form>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-main>
 
 
-                    <el-col :span="22" style="margin-bottom:24px;">
-                        <el-form label-width="auto" size="large">
-                            <el-input type="textarea" v-model="prompt" placeholder="Input text here. 在这里输入内容"
-                                rows="3"></el-input></el-form>
+            <el-list>
+                <el-list-item v-for="(chat, index) in chatList" :key="index">
 
-                    </el-col>
-                    <el-col :span="2" justify="end">
+                    <!--判断消息是从自己发出还是接收-->
+                    <template v-if="chat.from === 'self'">
+                        <el-row :gutter="8" justify="end" style="margin-top: 15px;">
 
-                        <el-button size="large" type="success" icon="Search" @click="submitForm">
-                        </el-button>
-                    </el-col>
+                            <el-card>
+                                <span style="white-space: pre-wrap; margin: 8px;">{{ chat.text }}</span>
+                            </el-card>
+                            <el-avatar style="margin-left:4px;" icon="UserFilled" />
+                        </el-row>
+                    </template>
+                    <template v-else>
+                        <el-row :gutter="8" justify="start" style="margin-top: 15px;">
+                            <el-avatar size="mini" style="margin-right: 4px;">
+                                <img :src="chat.face" />
+                            </el-avatar>
+                            <el-card>
+                                <span style="white-space: pre-wrap; margin: 8px;">{{ chat.text }}</span>
+                            </el-card>
 
-                </el-row>
-            </el-footer>
-        </el-container>
+                        </el-row>
+                    </template>
+
+                </el-list-item>
+            </el-list>
+        </el-main>
+        <el-footer>
+            <el-row justify="end">
+                <el-col :span="22" style="margin-bottom:24px;">
+                    <el-form label-width="auto" size="large">
+                        <el-input type="textarea" v-model="prompt" placeholder="Input text here. 在这里输入内容"
+                            rows="3"></el-input></el-form>
+                </el-col>
+                <el-col :span="2" justify="end">
+
+                    <el-button size="large" type="success" icon="Search" @click="submitForm">
+                    </el-button>
+                </el-col>
+
+            </el-row>
+        </el-footer>
     </el-container>
+
 </template>
 
 <script>
@@ -99,7 +98,7 @@ export default {
         if (temp_key != null)
             if (temp_key != '') {
                 this.api_key = localStorage.getItem('api-key')
-            }        
+            }
     },
     methods: {
 
